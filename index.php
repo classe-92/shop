576423d83cc25b270decef52ec3a69d365af10a4
klelia <clelia.fradella@gmail.com>
2023-05-14 22:02:53 +0200

    milestone 1

--- index.php
+++ index.php
@@ -3,8 +3,10 @@
 // https://www.php.net/manual/en/function.session-status.php
 // if (session_status() === PHP_SESSION_NONE) {
 //     session_start();
-// } 
-session_start();
+// }
+
+//include __DIR__ . '/partials/server/server.php';
+include __DIR__ . '/partials/Models/Book.php';
 if (!isset($_SESSION['userId'])) {
     //https: //www.php.net/manual/en/function.session-destroy.php
     if (ini_get("session.use_cookies")) {
@@ -18,11 +20,46 @@ if (!isset($_SESSION['userId'])) {
     }
     session_destroy();
     header("location: login.php");
+} else {
+    // $sql = "SELECT `title`, `cover_image`,`plot` FROM books";
+    // echo $sql;
+    // $result = $conn->query($sql);
+    $fields = ['title', 'cover_image', 'plot'];
+    $result = Book::fetchAll($conn, $fields, 'books');
+
+    // $book = Book::fetchOne($conn, 5, 'books', 'Book');
+    // var_dump($book);
+    $conn->close();
 }
+
 include './partials/template/header.php';
 ?>
 
-<h1>Titolo index</h1>
+<h1>Books</h1>
+<div class="row">
+    <?php
+    if ($result && $result->num_rows > 0) {
+        // la query è andata a buon fine e ci sono delle righe di risultati
+        //$result->fetch_assoc() - fetch_object('Book')
+        while ($row = $result->fetch_object('Book')) {
+            ?>
+            <div class="col-12 col-sd-6 col-md-4 col-lg-3">
+                <div class="card">
+                    <img src="<?php echo $row->cover_image ?>" class="card-img-top" alt="<?php echo $row->title ?>">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            <?php echo $row->title ?>
+                        </h5>
+                        <p class="card-text">
+                            <?php echo $row->getPlot() ?>
+                        </p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        <?php }
+    } ?>
+</div>
 
 <?php
 include './partials/template/footer.php';
